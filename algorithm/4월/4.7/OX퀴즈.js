cb301fdb9b7d77df66f79cec120a9c6ebbb2eb8f
dlgustj <hs508901@naver.com>
2024-04-07 23:54:48 +0900

    4.7 알고리즘

---
+++ algorithm/4월/4.7/OX퀴즈.js
@@ -0,0 +1,33 @@
+function solution(quiz) {
+    const answer = []
+    quiz.map((e) => {
+        let arr = e.split(" ");
+        
+        if (arr[1] === "+"){
+            if (Number(arr[0])+Number(arr[2]) === Number(arr[4])){
+                answer.push("O");
+            } else {
+                answer.push("X");
+            }
+        } else {
+            if (Number(arr[0])-Number(arr[2]) === Number(arr[4])){
+                answer.push("O");
+            } else {
+                answer.push("X");
+            }
+        }
+    })
+    return answer;
+}
+
+// 남의 풀이
+function solution(quiz) {
+    var answer = [];
+    return quiz.map(t => {
+        const [calc, result] = t.split(' = ');
+        const sign = calc.includes('+') ? 1 : -1
+        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
+
+        return +a + (+b * sign) === +result ? 'O' : 'X'
+    });
+}
