48b212e4dbdcd67119340f5e99b60bcba1e9383b
dlgustj <hs508901@naver.com>
2024-07-09 21:15:22 +0900

    7.9 스택 / 큐 알고리즘

--- algorithm/data_structure/stack/queue/주식 가격.js
+++ algorithm/data_structure/stack/queue/주식 가격.js
@@ -1,4 +1,13 @@
-function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
+function solution(prices) {
+    var answer = [];
+    prices.map((value,index)=>{
+        count=0;
+        for (let i=index+1; i<prices.length; i++){
+            count++;
+            if(value > prices[i])
+                break;
+        }
+        answer.push(count);
+    })
     return answer;
 }
--- algorithm/data_structure/stack/queue/프로세스.js
+++ algorithm/data_structure/stack/queue/프로세스.js
@@ -1,25 +1,41 @@
 function solution(priorities, location) {
     var answer = 0;
-    let sort_arr = [...priorities].sort((a,b)=> b - a);
-    let index = priorities.indexOf(sort_arr[0]);
-    console.log(index);
-    
-    for(let i=0; ;){
-        if (index+1 == priorities.length)
-            index = -1;
-
-        if(location === index){
-            answer = ++i;
-            break;
+    let index=0;
+    for (let j=1; j<=priorities.length; index++) {
+        max = Math.max(...priorities);
+        if (index == priorities.length)
+            index = 0;
+        if (priorities[index] == max) {
+            if(index === location){
+                answer = j;
+                break;
+            }
+            j++;
+            priorities[index] = 0;
         }
-        else {
-            index = priorities.indexOf(sort_arr[++i],index+1);
-            console.log(index);
-        }
-        
     }
-    console.log(answer);
     return answer;
 }
 
-solution([1,1,9,1,1,1], 0)
+// 다른 풀이
+function solution(priorities, location) {
+    var list = priorities.map((t,i)=>({
+        my : i === location,
+        val : t
+    }));
+    //console.log(list);
+    var count = 0;        
+    while(true){
+        var cur = list.splice(0,1)[0]; 
+        // var cur = list.shift();
+        
+        //console.log(cur);     
+        if(list.some(t=> t.val > cur.val )){
+            list.push(cur);                        
+        }
+        else{            
+            count++;
+            if(cur.my) return count;
+        }
+    }
+}
